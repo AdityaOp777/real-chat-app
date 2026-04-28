9be8df3acdc2db601cfd6a00b1e461eb5015bd01
AdityaOp777 <adityagavhane91@gmail.com>
2026-04-28 22:26:12 +0530

    FullStack Real-Chat-App Spring Boot Project

---
+++ real-time-chat/src/test/java/com/aditya/real_time/chat/RealTimeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.real_time.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
